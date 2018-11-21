87db3d98bb176712a42bcae953567d347cb60083
Matheus Paiva <matheus.a.paiva@gmail.com>
2018-11-21 14:42:16 -0200

    refactor(rect): make coordinates attributes float
    Make Rx and Ry coordinates, Width and  Heigh attributes float64. Moreover, add two more coordinate attributes: .

--- rect.go
+++ rect.go
@@ -3,13 +3,16 @@ package svg
 import mt "github.com/rustyoz/Mtransform"
 
 type Rect struct {
-	Id        string `xml:"id,attr"`
-	Width     string `xml:"width,attr"`
-	Height    string `xml:"height,attr"`
-	Transform string `xml:"transform,attr"`
-	Style     string `xml:"style,attr"`
-	Rx        string `xml:"rx,attr"`
-	Ry        string `xml:"ry,attr"`
+	Id        string  `xml:"id,attr"`
+	Width     float64 `xml:"width,attr"`
+	Height    float64 `xml:"height,attr"`
+	Transform string  `xml:"transform,attr"`
+	Style     string  `xml:"style,attr"`
+	Rx        string  `xml:"rx,attr"`
+	Ry        string  `xml:"ry,attr"`
+	X         float64 `xml:"x,attr"`
+	Y         float64 `xml:"y,attr"`
+	Title     string  `xml:"title"`
 
 	transform mt.Transform
 	group     *Group
